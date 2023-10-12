fa658450bdd1386879f43c804e5e5bee938e9763
jeeya-karki <jeeyakarki2004@gmail.com>
2023-10-12 13:26:46 +0530

    Today's hw,10-10-23

--- Ancient_Egyptian(While_loop-HW).py
+++ Ancient_Egyptian(While_loop-HW).py
@@ -1,14 +1,9 @@
-a=23
-b=33
-while(b<=0):
-    a=a*2
-    print(a)
-    b=b/2
-    print(b)
-
-    a=a+1
-    b=b+1
-
-print(a)
-print(b)
-
+a=int(input("Enter number1:"))
+b=int(input("Enter number2:"))
+sum=0
+while b!=1:
+    a*=2;
+    b/=2;
+    if(b%2!=0):
+            sum=a;
+print("sum is:",sum)
