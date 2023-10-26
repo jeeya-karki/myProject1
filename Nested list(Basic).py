a0f37e8a71f1f27e53a76d33574d0fe27a2dd4b6
jeeya-karki <jeeyakarki2004@gmail.com>
2023-10-26 16:55:04 +0530

    Nested list H.W

--- Nested list(Basic).py
+++ Nested list(Basic).py
@@ -1,14 +1,14 @@
 list1=[ [251,399,832]  , [402,36,187] ]
 
- print(list1)
- print(list1[0])
- print(list1[0][0])
- print(list1[1][2])
- print("=================================")
- for element in list1:
+print(list1)
+print(list1[0])
+print(list1[0][0])
+print(list1[1][2])
+print("=================================")
+for element in list1:
      print(element)
- print("=================================")
- for element in list1:
+print("=================================")
+for element in list1:
      for value in element:
          print(value)
 
